499d4258100027aa6658338c8a88bc1ab9d3f961
John Merz <jmerz826@gmail.com>
2021-12-20 18:03:29 -0500

    built basic axiosWithAuth util

---
+++ src/components/logout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from "react-router-dom";
+
+const Logout = (props) => {
+    const { push } = useHistory();
+    return <div></div>
+}
+
+export default Logout;
---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+// Temporary axiosWithAuth, subject to change based on backend functionality. - John
+const axiosWithAuth = () => {
+    const token = localStorage.getItem('token');
+
+    // Returns axios element containing the storage from local storage and declaring the base URL for CRUD methods.
+    return axios.create({
+        headers: {
+            authorization: token
+        },
+        // baseURL: 'http://localhost:5000/api'
+    });
+};
+
+export default axiosWithAuth;
