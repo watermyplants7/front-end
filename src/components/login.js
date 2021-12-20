52738e04616805c7aae06286861182b911fad9a0
iamleahb <leah.ball@gmail.com>
2021-12-20 16:15:34 -0600

    test push

--- src/components/login.js
+++ src/components/login.js
@@ -1,51 +1,56 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyleLogin = styled.div`
+  background-color: red;
+`;
 
 const initialFormValues = {
-    phone: '',
-    password:''
-}
+  phone: "",
+  password: "",
+};
 
 const Login = (props) => {
-    const [formValues, setFormValues] = useState(initialFormValues);
+  const [formValues, setFormValues] = useState(initialFormValues);
 
-    const handleChange = (e) => {
-        setFormValues({
-            ...formValues,
-            [e.target.name]: e.target.value
-        });
-    };
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // API post once we have endpoint
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // API post once we have endpoint
+  };
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <form>
-                <label>
-                    Phone Number:
-                    <input
-                        type='tel'
-                        name='phone'
-                        value={formValues.phone}
-                        onChange={handleChange}
-                    />
-                </label>
-                <label>
-                    Password:
-                    <input
-                        type='password'
-                        name='password'
-                        value={formValues.password}
-                        onChange={handleChange}
-                    />
-                </label>
-                <button onClick={handleSubmit}>Log in</button>
-            </form>
-        </div>
-    );
+  return (
+    <StyleLogin>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Phone Number:
+          <input
+            type="tel"
+            name="phone"
+            value={formValues.phone}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formValues.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button onClick={handleSubmit}>Log in</button>
+      </form>
+    </StyleLogin>
+  );
 };
 
 export default Login;
