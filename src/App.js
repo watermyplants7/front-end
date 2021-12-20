45c67b49bc7fd775bb9a55f88123f2fd67a1a107
John Merz <jmerz826@gmail.com>
2021-12-20 16:51:35 -0500

    login form built out

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Register from './components/Register';
 import LandingPage from './components/landingPage/landingPage';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>Replace this element with Header component</h1>
       <Router>
         <Switch>
           <Route path='/register'>
             <Register />
+          </Route>
+          <Route path='/login'>
+            <Login />
           </Route>
           <Route exact path='/'>
             <div>pre-login home page</div>
