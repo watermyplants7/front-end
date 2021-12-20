fd2a28b2c666718a58b82dadc7c7b5326a259e21
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-20 14:27:53 -0800

    Basic add/edit plant form structure built

---
+++ src/components/EditPlant.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const EditPlant = () => {
+    return (
+        <div>
+            <div className='plant-form-container'>
+                <h2>Edit a Plant</h2>
+                <form className='plant-form'>
+                    <div>
+                        <label>Photo URL (Optional)</label>
+                        <input type='text' name='photo' />
+                    </div>
+                    <div>
+                        <label>Nickname</label>
+                        <input type='text' name='nickname' />
+                    </div>
+                    <div>
+                        <label>Species</label>
+                        <input type='text' name='species' />
+                    </div>
+                    <div>
+                        <label>Water Frequency</label>
+                        <select>
+                            <option value='daily'>Daily</option>
+                            <option value='every-other-day'>Every Other Day</option>
+                            <option value='twice-weekly'>Twice Weekly</option>
+                            <option value='weekly'>Weekly</option>
+                            <option value='bi-weekly'>Bi-Weekly</option>
+                            <option value='Monthly'>Monthly</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label>Care Instructions</label>
+                        <input type='text' name='care-instructions' />
+                    </div>
+                    <button>Update Plant</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default EditPlant;
