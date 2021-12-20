62b6798ea8f0805b20a599eee7f35a6da6a31383
John Merz <jmerz826@gmail.com>
2021-12-20 18:13:11 -0500

    built out logout component. done pending proper axiosWithAuth build.

--- src/components/logout.js
+++ src/components/logout.js
@@ -4,7 +4,22 @@ import { useHistory } from "react-router-dom";
 
 const Logout = (props) => {
     const { push } = useHistory();
-    return <div></div>
+
+    const handleLogout = () => {
+        // axiosWithAuth().post('/logout').then(res => {
+        //     localStorage.removeItem('token');
+        //     push('/');
+        // }).catch(err => {
+        //     console.error(err);
+        // })
+        push('/');
+    };
+
+    return <div>
+        <h2>We're sorry to see you go!</h2>
+        <h3>Are you sure you want to log out?</h3>
+        <button onClick={handleLogout}>Yes, log me out</button>
+    </div>
 }
 
 export default Logout;
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -9,7 +9,7 @@ const axiosWithAuth = () => {
         headers: {
             authorization: token
         },
-        // baseURL: 'http://localhost:5000/api'
+        baseURL: 'http://localhost:5000/api'
     });
 };
 
