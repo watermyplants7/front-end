64c7db88c58b61d60df890a936f055a8cb98bf36
John Merz <jmerz826@gmail.com>
2021-12-20 14:34:10 -0500

    basic routing, folder build out

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route exact path='/'>
           <div>pre-login home page</div>
         </Route>
+        <PrivateRoute path='/home' component={<div><h1>logged in home page component will go here</h1></div>}/>
 
       </Switch>
     </div>
