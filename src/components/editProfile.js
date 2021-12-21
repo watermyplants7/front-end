2b95339b5ec466ebaf0372fa6656efa56824e08f
John Merz <jmerz826@gmail.com>
2021-12-21 14:05:27 -0500

    building out edit profile feature

---
+++ src/components/editProfile.js
@@ -0,0 +1,77 @@
+import React, {useState, useEffect} from "react";
+
+
+const EditProfile = (props) => {
+    const [formValues, setFormValues] = useState('');
+
+    const handleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    useEffect(() => {
+        const username = localStorage.getItem('username');
+        const phone = localStorage.getItem('phone');
+        const email = localStorage.getItem('email');
+        const password = localStorage.getItem('password');
+        setFormValues({
+            username: username,
+            phone: phone, 
+            email: email,
+            password:password
+        })
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+    return (
+        <div>
+            <form>
+                <h2>Edit your Profile Information</h2>
+                <label>
+                    Username:
+                    <input
+                        type='text'
+                        name='username'
+                        value={formValues.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Phone Number:
+                    <input
+                        type='tel'
+                        name='phone'
+                        value={formValues.phone}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                        type='email'
+                        name='email'
+                        value={formValues.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type='password'
+                        name='username'
+                        value={formValues.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button onClick={handleSubmit}>Update info!</button>
+            </form>
+        </div>
+    );
+};
+
+export default EditProfile;
