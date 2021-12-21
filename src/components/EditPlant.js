e6ea3e26301443211801fe3f190ac6054e5306da
John Merz <jmerz826@gmail.com>
2021-12-21 13:22:26 -0500

    pulled from main

--- src/components/EditPlant.js
+++ src/components/EditPlant.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -89,48 +88,5 @@ const EditPlant = () => {
         </div>
     )
 }
-=======
-import React from "react";
-
-const EditPlant = () => {
-  return (
-    <div>
-      <div className="plant-form-container">
-        <h2>Edit a Plant</h2>
-        <form className="plant-form">
-          <div>
-            <label>Photo URL (Optional)</label>
-            <input type="text" name="photo" />
-          </div>
-          <div>
-            <label>Nickname</label>
-            <input type="text" name="nickname" />
-          </div>
-          <div>
-            <label>Species</label>
-            <input type="text" name="species" />
-          </div>
-          <div>
-            <label>Water Frequency</label>
-            <select>
-              <option value="daily">Daily</option>
-              <option value="every-other-day">Every Other Day</option>
-              <option value="twice-weekly">Twice Weekly</option>
-              <option value="weekly">Weekly</option>
-              <option value="bi-weekly">Bi-Weekly</option>
-              <option value="Monthly">Monthly</option>
-            </select>
-          </div>
-          <div>
-            <label>Care Instructions</label>
-            <input type="text" name="care-instructions" />
-          </div>
-          <button>Update Plant</button>
-        </form>
-      </div>
-    </div>
-  );
-};
->>>>>>> 1d2433f807bfed46b75545276135363829af87c4
 
 export default EditPlant;
