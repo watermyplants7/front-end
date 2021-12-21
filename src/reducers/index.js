723275cdc9d9377262620865ebc49ca8109f66f3
John Merz <jmerz826@gmail.com>
2021-12-21 13:17:39 -0500

    console logs for reducer cases, started useEffect call for mount

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,16 +9,19 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case (ADD_PLANT):
+            console.log('Reducer case: ADD_PLANT triggered')
             return ({
                 ...state,
                 plants: [...state.plants, action.payload]
             });
         case (EDIT_PLANT):
+            console.log('Reducer case: EDIT_PLANT triggered')
             return ({
                 ...state,
                 // NOT FINISHED, NEED API ENDPOINT
             });
         case (DELETE_PLANT):
+            console.log('Reducer case: DELETE_PLANT triggered')
             return ({
                 ...state,
                 // NOT FINISHED, NEED API ENDPOINT
