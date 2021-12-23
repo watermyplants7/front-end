487eb5c56862f4fe2f89fbf2e916552805c22b06
iamleahb <leah.ball@gmail.com>
2021-12-23 15:58:37 -0600

    created styles for login page

--- src/Theme/Colors.js
+++ src/Theme/Colors.js
@@ -1,4 +1,5 @@
 export const MainGreen = "#4d9973";
 export const SecondaryGreen = "#579457";
 export const SecondaryBlue = "#4d7399";
-export const Contrast = "#674589";
+export const Contrast = "#9f329e";
+export const LightGreen = "#f1f6f1";
